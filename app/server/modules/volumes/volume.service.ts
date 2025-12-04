9ba26b7599b3c316e9e088d770a72b6ce8697f09
Nicolas Meienberger <github@thisprops.com>
2025-12-04 19:09:23 +0100

    feat: add DOCKER_HOST support

--- app/server/modules/volumes/volume.service.ts
+++ app/server/modules/volumes/volume.service.ts
@@ -5,7 +5,7 @@ import Docker from "dockerode";
 import { and, eq, ne } from "drizzle-orm";
 import { ConflictError, InternalServerError, NotFoundError } from "http-errors-enhanced";
 import slugify from "slugify";
-import { getCapabilities } from "../../core/capabilities";
+import { getCapabilities, parseDockerHost } from "../../core/capabilities";
 import { db } from "../../db/db";
 import { volumesTable } from "../../db/schema";
 import { cryptoUtils } from "../../utils/crypto";
@@ -298,7 +298,8 @@ const getContainersUsingVolume = async (name: string) => {
 	}
 
 	try {
-		const docker = new Docker();
+		const docker = new Docker(parseDockerHost(process.env.DOCKER_HOST));
+
 		const containers = await docker.listContainers({ all: true });
 
 		const usingContainers = [];
